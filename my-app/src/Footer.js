8daab04a39848ce6b64db278f1749f7cf15cf3a3
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-08-17 18:25:07 +0800

    practicing using react

---
+++ my-app/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 20224 My Website. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
